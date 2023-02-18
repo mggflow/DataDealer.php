9d944402709d986f9fc2c853f0d6ba02bbc02eba
Deusagnis <deusagnis@gmail.com>
2023-02-18 13:40:50 +0300

    Change urls regular expression.

--- src/Entities/Regular.php
+++ src/Entities/Regular.php
@@ -4,5 +4,5 @@ namespace MGGFLOW\DataDealer\Entities;
 
 class Regular
 {
-    const URL_REGEX = "(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})";
+    const URL_REGEX = "/https?:\\/\\/(?:www\\.)?[-a-zA-Z\d@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)/";
 }
